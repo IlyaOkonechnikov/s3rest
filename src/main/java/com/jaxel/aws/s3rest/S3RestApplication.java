5b907b89a44ae76fe00236b2548e08073154d43c
Ilya Okonechnikov <ok.ilya95kazan@mail.ru>
2021-10-15 18:11:27 +0200

    initial commit

---
+++ src/main/java/com/jaxel/aws/s3rest/S3RestApplication.java
@@ -0,0 +1,13 @@
+package com.jaxel.aws.s3rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaxel/aws/s3rest/S3RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaxel.aws.s3rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
